b482a9ee016158141c6bd5d3b5f21fc246150516
yusaku-terashima <terassy0809@docomo.ne.jp>
2024-06-10 15:39:51 +0900

    登録されるパスワードのハッシュ化

--- src/main/java/com/example/service/AdministratorService.java
+++ src/main/java/com/example/service/AdministratorService.java
@@ -1,6 +1,7 @@
 package com.example.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -19,25 +20,39 @@ public class AdministratorService {
     @Autowired
     private AdministratorRepository administratorRepository;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     /**
      * 管理者情報を登録します.
      *
      * @param administrator 管理者情報
      */
+//    public void insert(Administrator administrator) {
+//        administratorRepository.insert(administrator);
+//    }
     public void insert(Administrator administrator) {
+
+        administrator.setPassword(passwordEncoder.encode(administrator.getPassword()));
+
         administratorRepository.insert(administrator);
     }
 
     /**
      * ログインをします.
+     * ハッシュ化したパスワードと一致した場合ログイン。
      *
      * @param mailAddress メールアドレス
      * @param password    パスワード
      * @return 管理者情報 存在しない場合はnullが返ります
      */
     public Administrator login(String mailAddress, String password) {
-        Administrator administrator = administratorRepository.findByMailAddressAndPassward(mailAddress, password);
-        return administrator;
+        Administrator administrator = administratorRepository.findByMailAddress(mailAddress);
+        if(passwordEncoder.matches(password, administrator.getPassword())){
+            return administrator;
+        }else{
+            return null;
+        }
     }
 
 	/**
